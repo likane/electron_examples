bc512daa34c9e1a2701a10d4aaafb161cda63dd8
Louis Kane <louiskane@Louiss-MBP-2.lan>
2024-03-25 21:04:30 -0400

    stylized components

--- notesapp/app/src/components/Layout.tsx
+++ notesapp/app/src/components/Layout.tsx
@@ -1,15 +1,14 @@
-import { Outlet } from "react-router-dom"
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import { NavBar } from './Navbar';
 
 export const Layout: React.FC = () => {
-    return (
-        <div>
-            <p>Layout</p>
-            <ul>
-                <li>
-                    <a href="/work">work</a>
-                </li>
-            </ul>
-            <Outlet/>
-        </div>
-    )
-}
+  return (
+    <div className="mx-auto max-w-4xl">
+      <NavBar />
+      <div className="py-2 px-6">
+        <Outlet />
+      </div>
+    </div>
+  );
+};
--- notesapp/app/src/index.tsx
+++ notesapp/app/src/index.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import {Layout} from './components/Layout'
-import {Work} from './pages/Work'
+import { Layout } from './components/Layout';
+import { Work } from './pages/Work';
+import DocumentUploadPage from './pages/Document/Upload';
+import DocumentIndex from './pages/Document/Index';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    {/* <App /> */}
-    <BrowserRouter>
+  // <React.StrictMode>
+  <BrowserRouter>
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route path="/work" element={<Work/>} />
+        <Route path="/work" element={<Work />} />
+        <Route path="/document/upload" element={<DocumentUploadPage />} />
+        <Route path="/document/index" element={<DocumentIndex />} />
       </Route>
     </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+  </BrowserRouter>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
